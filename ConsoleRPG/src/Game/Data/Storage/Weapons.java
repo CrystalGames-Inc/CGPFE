83b13ce0c71c584037d3349081be3ea731344ffc
CrystalGames2000 <ofirabe09@gmail.com>
2024-12-13 23:57:36 +0200

    Added a new weapon
    Spiked Gauntlet

--- ConsoleRPG/src/Game/Data/Storage/Weapons.java
+++ ConsoleRPG/src/Game/Data/Storage/Weapons.java
@@ -7,4 +7,5 @@ public class Weapons {
     public Weapon gauntlet = new Weapon("Gauntlet", 0, 1, 0, 2, "1d2", "1d3", "x2", 0.0, 1.0, new Types[]{Types.BLUDGEONING}, null);
     public Weapon dagger = new Weapon("Dagger", 0, 1, 0, 2, "1d3", "1d4","19-20/x2",10.0, 1.0, new Types[]{Types.PIERCING, Types.SLASHING, Types.OR}, null);
     public Weapon punchingDagger = new Weapon("Punching Dagger", 0, 1, 0, 2, "1d3", "1d4","x3", 0.0, 1, new Types[]{Types.PIERCING}, null);
+    public Weapon spikedGauntlet = new Weapon("Spiked Gauntlet", 0, 1, 0, 5, "1d3","1d4","x2", 0.0, 1.0, new Types[]{Types.PIERCING}, null);
 }
