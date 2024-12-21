362e28f80e054b360fd4c03ee446c3f3b1f64b37
CrystalGames2000 <ofirabe09@gmail.com>
2024-12-21 22:39:26 +0200

    Added All Adventuring Gear Goods
    Time To Go To Sleep

--- ConsoleRPG/src/Game/Data/Storage/World/Goods.java
+++ ConsoleRPG/src/Game/Data/Storage/World/Goods.java
@@ -3,6 +3,9 @@ package Game.Data.Storage.World;
 import Game.Data.Models.ItemTypes.Goods.Good;
 
 public class Goods {
+
+    //region Adventuring Gear
+
     public Good backpack = new Good("Backpack",0,0,0,2,2);
     public Good barrel = new Good("Barrel",1,0,0,2,30);
     public Good basket = new Good("Basket",2,0,0,2,1);
@@ -11,4 +14,66 @@ public class Goods {
     public Good winterBlanket = new Good("Winter Blanket",5,0,0,0.5,3);
     public Good blockAndTackle = new Good("Block And Tackle",6,0,0,5,5);
     public Good glassBottle = new Good("Glass Bottle",7,0,0,2,1);
+    public Good bucket = new Good("Bucket",8,0,0,0.5,2);
+    public Good caltrops = new Good("Caltrops",9,0,0,1,2);
+    public Good candle = new Good("Candle",10,0,0,0.01,0);
+    public Good canvas = new Good("Canvas",11,0,0,0.1,1);
+    public Good CMP = new Good("Case, Map Or Scroll",12,0,0,1,0.5);
+    public Good chain = new Good("Chain",13,0,0,30,2);
+    public Good chalk = new Good("Chalk",14,0,0,1,0);
+    public Good chest = new Good("Chest",15,0,0,2,25);
+    public Good crowbar = new Good("Crowbar",16,0,16,2,5);
+    public Good firewood = new Good("Firewood",17,0,0,1,20);
+    public Good fishhook = new Good("Fishhook",18,0,0,0.1,0);
+    public Good fishingNet = new Good("Fishing Net",19,0,0,4,5);
+    public Good flask = new Good("Flask",20,0,0,3,0.5);
+    public Good flintAndSteel = new Good("Flint And Steel",21,0,0,1,0);
+    public Good grapplingHook = new Good("Grappling Hook",22,0,0,1,4);
+    public Good hammer = new Good("Hammer", 23,0,0,0.5,2);
+    public Good hourglass = new Good("Hourglass",24,0,0,25,1);
+    public Good inkVial = new Good("Ink Vial",25,0,0,8,0);
+    public Good inkPen = new Good("Ink Pen",26,0,0,0.1,0);
+    public Good clayJug = new Good("Clay Jug",27,0,0,0.03,9);
+    public Good ladder = new Good("Ladder",28,0,0,0.2,20);
+    public Good commonLamp = new Good("Common Lamp",29,0,0,0.1,1);
+    public Good bullseyeLantern = new Good("Bullseye Lantern", 30,0,0,12,3);
+    public Good hoodedLantern = new Good("Hooded Lantern",31,0,0,7,2);
+    public Good simpleLock = new Good("Lock(Simple)",32,0,0,20,1);
+    public Good averageLock = new Good("Lock(Average)",33,0,0,20,1);
+    public Good goodLock = new Good("Lock(Good)",34,0,0,20,1);
+    public Good superiorLock = new Good("Lock(Superior)",35,0,0,20,1);
+    public Good manacles = new Good("Manacles",36,0,0,15,2);
+    public Good masterworkManacles = new Good("Manacles(Masterwork)",37,0,0,50,2);
+    public Good smallSteelMirror = new Good("Small Steel Mirror",38,0,0,10,0.5);
+    public Good clayMug = new Good("ClayMug",39,0,0,0.02,1);
+    public Good oil = new Good("Oil",40,0,0,.01,1);
+    public Good paperSheet = new Good("Paper(Sheet)",41,0,0,0.4,0);
+    public Good parchmentPaper = new Good("Parchment(Sheet)",42,0,0,0.2,0);
+    public Good minersPick = new Good("Miner's Pick",43,0,0,3,10);
+    public Good clayPitcher = new Good("Clay Pitcher",44,0,0,0.02,5);
+    public Good piton = new Good("Piton",45,0,0,0.1,0.5);
+    public Good pole = new Good("Pole(10ft)",46,0,0,0.05,8);
+    public Good ironPot = new Good("Iron Pot",47,0,0,0.8,4);
+    public Good beltPouch = new Good("Belt Pouch",48,0,0,1,0.5);
+    public Good portableRam = new Good("Portable Ram",49,0,0,10,20);
+    public Good trailRations = new Good("Trail Rations",50,0,0,0.5,1);
+    public Good hempRope = new Good("Rope Hemp",51,0,0,1,10);
+    public Good silkRope = new Good("Silk Rope",52,0,0,10,5);
+    public Good sack = new Good("Sack",53,0,0,0.1,0.5);
+    public Good sealingWax = new Good("Sealing Wax",54,0,0,1,1);
+    public Good sewingNeedle = new Good("Sewing Needle",55,0,0,0.5,0);
+    public Good shovelSpade = new Good("ShovelOrSpade",56,0,0,2,8);
+    public Good signalWhistle = new Good("Signal Whistle",57,0,0,0.8,0);
+    public Good signetRing = new Good("Signet Ring",58,0,0,5,0);
+    public Good sledge = new Good("Sledge",59,0,0,1,10);
+    public Good soap = new Good("Soap",60,0,0,0.5,1);
+    public Good spyglass = new Good("Spyglass",61,0,0,1000,1);
+    public Good tent = new Good("Tent", 62,0,0,10,20);
+    public Good torch = new Good("Torch",63,0,0,0.01,1);
+    public Good vial = new Good("Vial(Ink Or Potion)",64,0,0,1,0);
+    public Good waterClock = new Good("Water Clock",65,0,0,1000,200);
+    public Good waterskin = new Good("Waterskin",66,0,0,1,4);
+    public Good whetstone = new Good("Whetstone",67,0,0,0.02,1);
+
+    //endregion
 }
